86b538686a6ceb7724c7d3b7fbe283bc3522ccc5
MauRS1005 <mauricio.rojas10@hotmail.com>
2022-07-11 01:14:39 -0600

    Se agrega el modelo Empleado

---
+++ db/migrate/20220711071126_create_employees.rb
@@ -0,0 +1,11 @@
+class CreateEmployees < ActiveRecord::Migration[7.0]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :firstName
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/controllers/employees_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class EmployeesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
