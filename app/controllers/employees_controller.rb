712dd7a5e30dede7eba60c4a0fa682befbdbc91a
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.com>
2022-07-11 01:37:05 -0600

    Se agregan vistas de creación de empleado

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,23 @@
 class EmployeesController < ApplicationController
+
+    # GET
+    def index
+        @employees = Employee.all
+    end
+
+    # GET
+    def new 
+        @employee = Employee.new
+    end
+
+    # POST
+    def create
+    end
+
+    # GET
+    def update
+
+    end
+    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index"
+
+  resources :employees, only: [:index, :new, :create]
 end
