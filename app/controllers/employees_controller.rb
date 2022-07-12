01be0f2f4b5eaaf98747dba866fd6c161a200490
AndresUCRSO <andresisaac.lopez@ucr.ac.cr>
2022-07-11 22:25:05 -0600

    Se agrega el metodo POST de empleados

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -12,12 +12,21 @@ class EmployeesController < ApplicationController
 
     # POST
     def create
+        @employee = Employee.new(employee_params)
+        if @employee.save
+            redirect_to employees_path, notice: "Employee created successfully"
+        else
+            render :new, status: :unprocessable_entity
+        end 
     end
 
     # GET
     def update
 
     end
-    
 
+    private 
+    def employee_params
+        params.require(:employee).permit(:name, :phone)
+    end
 end
