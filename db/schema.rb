4e720f3ec4769510be49ca469b3c9e28af85aaf8
Carlos Daniel Vargas Varela <carlos.vargas@ucrso.com>
2022-07-11 21:16:46 -0600

    Se solucionan problemas en migraciones

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_11_071126) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_12_031555) do
   create_table "employees", force: :cascade do |t|
     t.string "name"
-    t.string "firstName"
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
